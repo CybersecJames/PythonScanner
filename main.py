9964822546d92e6dde9d1fdef878ec3f840570bf
James Montrief <james.montrief@gmail.com>
2021-09-26 01:53:07 -0400

    created setup.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import random
+# import statements
 import socket
 
-import settings as hf
 import pyfiglet
 from consolebar import ConsoleBar
 
-global tgt
-global timer
-
-hf.clear()
+# import the local settings file
+import settings as hf
 
 
 # --------------------------------------------------------------------------------
@@ -20,97 +17,145 @@ hf.clear()
 #        |___/
 # --------------------------------------------------------------------------------
 
-
-# basic project info
+# ------------------------------------- PROJECT INFO ------------------------------------------ #
+# general project information
 class proj_info:
     title = 'Python Scanner'
     dev = 'James Montrief'
-    description = 'Python Scanner is a simple port scanning application written in python'
+    description = 'Python Scanner is a simple port scanning application written in python.'
 
 
-# prints the home screen
-def home_screen():
+# ---------------------------------------- BANNERS -------------------------------------------- #
+# set up the banner class
+class banner:
+    def __init__(self, name, header, subtitle, message):
+        self.name = name
+        self.header = header
+        self.subtitle = subtitle
+        self.message = message
+
+
+# welcome banner
+welcome_banner = banner(
+    name='Welcome Banner',
+    header=proj_info.title,
+    subtitle=hf.break_line + '\n' + proj_info.title + ' | ' + 'Developed by: ' + proj_info.dev + '\n' + hf.break_line,
+    message=proj_info.description,
+)
+
+# small banner
+small_banner = banner(
+    name='Small Banner',
+    header=proj_info.title,
+    subtitle=proj_info.title + ' | ' + 'Developed by: ' + proj_info.dev,
+    message=proj_info.description,
+)
+
+
+def simple_banner():
+    pyfiglet.print_figlet(proj_info.title, colors=banner_color)
+    print(welcome_banner.subtitle)
+
+
+# banner colors
+banner_color = 'GREEN'
+banner_font = 'DOOM'
+
+
+# ---------------------------------------- WElCOME SCREEN -------------------------------------------- #
+# sets the welcome screen
+def welcome_screen():
+    pyfiglet.print_figlet(text=proj_info.title, colors=banner_color)
+    print(welcome_banner.subtitle)
+
+    def pass_screen():
+        msg = '\nPress [ENTER] to continue... \n'
+        input(msg)
+
+    pass_screen()
+
     hf.clear()
 
-    def randomizer():
-        fonts = ['doom']
-        colors = ['GREEN']
 
-        random_font = random.choice(fonts)
-        random_color = random.choice(colors)
-        pyfiglet.print_figlet(proj_info.title, font=random_font, colors=random_color)
-        hf.break_line(80)
-        print(proj_info.title + ' | Developed by: ' + proj_info.dev)
-        hf.break_line(80)
-
-    randomizer()
-
-
-# similar to main banner, but removes the project information
-def mini_banner():
-    pyfiglet.print_figlet(proj_info.title, font='doom', colors='GREEN')
-    hf.break_line(80)
-
-
+# -------------------------------------- TARGET ACQUISITION ------------------------------------------ #
 # defining the target class
 class target:
-    def __init__(self, hostname):
+    def __init__(self, hostname, start_port, end_port):
         self.hostname = hostname
+        self.start_port = start_port
+        self.end_port = end_port
         self.ip = socket.gethostbyname(hostname)
         self.fqdn = socket.getfqdn(hostname)
 
 
-# getting the target
-def target_acquisition():
-    print('\nEnter a hostname (e.g. abc.com) : \n')
-    raw = input(' >> ')
-
-    # '99' resets the program
-    if raw == '99':
-        hf.clear()
-        main()
-
-    # carry on
-    else:
-        global tgt
-        try:
-            # turns the raw target
-            tgt = target(raw)
-            hf.clear()
-            mini_banner()
-            print('\nTarget: ' + raw)
-            print('IP address: ' + tgt.ip + '\n')
-            hf.break_line(80)
-            scanner()
-
-        except:
-            main()
-
-
-# the scanning engine
-def scanner():
-    open_ports = []
-    starting_port = int(input('Enter the starting port: '))
-    ending_port = int(input('Enter ending port: '))
-
+# reconnaissance
+def recon_target():
+    # acquire the target
+    simple_banner()
+    tgt_prompt = '\nEnter a hostname (e.g. abc.com): \n  >>  '
+    tgt = input(tgt_prompt)
     hf.clear()
 
-    mini_banner()
-    print('\nScanning target: ' + tgt.hostname)
-    print('IP address: ' + tgt.ip + '\n')
+    # starting port
+    simple_banner()
+    start_port_prompt = '\nEnter the starting port: '
+    start_port = int(input(start_port_prompt + '\n  >>  '))
+    hf.clear()
 
-    for port in ConsoleBar(range(starting_port, ending_port)):
+    # ending port
+    simple_banner()
+    end_port_prompt = '\nEnter the ending port: '
+    end_port = int(input(end_port_prompt + '\n  >>  '))
+    hf.clear()
+
+    # socket timeout
+    simple_banner()
+    sock_timeout_prompt = '\nEnter the scan speed: \n  1 - Fastest \n  2 - Medium \n  3 - Slow\n'
+    sock_timeout = int(input(sock_timeout_prompt + '\n  >>  '))
+    hf.clear()
+
+    def wait_screen():
+        simple_banner()
+        message = 'Conducting reconnaissance of the target, please wait...'
+        print(message)
+
+    wait_screen()
+
+    # recon
+    tgt = target(tgt, start_port, end_port)
+    hf.clear()
+    # ---------------------------------------- SCANNING ENGINE ------------------------------------------ #
+    # scanning engine
+    open_ports = []
+
+    # target information
+    simple_banner()
+    print('\nScanning target: ' + tgt.hostname)
+    print('IP address: ' + tgt.ip)
+    print('Scanning ports ' + str(start_port) + ' - ' + str(end_port) + '\n')
+
+    # scanning for ports
+    for port in ConsoleBar(range(start_port, end_port)):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.12)
+        sock.settimeout(sock_timeout / 5)
         result = sock.connect_ex((tgt.ip, port))
 
         if result == 0:
             try:
                 service = socket.getservbyport(port)
-                open_ports.append("Port {}  | Open".format(port) + '     | ' + service)
+
+                if port < 99:
+                    open_ports.append("Port {}      | Open".format(port) + '     | ' + service)
+
+                elif port > 99:
+                    open_ports.append("Port {}     | Open".format(port) + '     | ' + service)
 
             except:
-                open_ports.append("Port {}  | Open   ".format(port) + '  | ')
+                if port < 99:
+                    open_ports.append("Port {}      | Open".format(port) + '     | ')
+
+                elif port > 99:
+                    open_ports.append("Port {}     | Open".format(port) + '     | ')
 
             sock.close()
 
@@ -119,27 +164,30 @@ def scanner():
         hf.clear()
 
         print("REPORT: ")
+        print(hf.break_line)
 
-        def report_data():
-            hf.break_line(80)
-            hostname_report = 'Target:           ' + tgt.hostname
-            ip_report = 'IP address:       ' + tgt.ip
-            fqdn_report = 'FQDN:             ' + tgt.fqdn
+        hostname_report = 'Target:           ' + tgt.hostname
+        ip_report = 'IP address:       ' + tgt.ip
+        fqdn_report = 'FQDN:             ' + tgt.fqdn
 
-            print(hostname_report + '\n' + ip_report + '\n' + fqdn_report)
-            print('Port range:       ' + str(starting_port) + ' - ' + str(ending_port))
+        print(hostname_report + '\n' + ip_report + '\n' + fqdn_report)
+        print('Port range:       ' + str(start_port) + ' - ' + str(end_port))
 
-            print('\nPort       ' + 'Status    ' + 'Service ')
-            hf.break_line(80)
+        print('\nPort       ' + 'Status    ' + 'Service ')
+        print(hf.break_line)
 
-            print(*open_ports, sep='\n')
+        print(*open_ports, sep='\n')
 
-        report_data()
-
-        hf.break_line(80)
+        print(hf.break_line)
 
         # exit
-        input('\n' * 5 + 'Press any key to continue...')
+        rerun = input('\n' * 3 + 'Press [ENTER] to run another scan, or type "exit" to quit.\n')
+
+        if rerun == 'exit':
+            hf.clear()
+            print('Thank you for using Python Scanner.')
+            exit()
+
         hf.clear()
 
     report()
@@ -147,14 +195,11 @@ def scanner():
 
 # main loop
 def main():
-    home_screen()
-    target_acquisition()
-    scanner()
+    recon_target()
     main()
 
 
+welcome_screen()
+
 if __name__ == '__main__':
-    try:
-        main()
-    except:
-        print('Something has gone wrong. Please alert the developers by creating an issue on GitHub.')
+    main()
--- settings.py
+++ settings.py
@@ -1,26 +1,15 @@
 from os import system
 
-import pyfiglet
 
-
+# clear function
 def clear():
     system("clear")
 
 
 # break line
-def break_line(dashes):
+
+break_line = '--------------------------------------------------------------------------------'
+
+
+def break_line_alt(dashes):
     print('-' * dashes)
-
-
-class banner:
-    def __init__(self, name, header, message):
-        self.name = name
-        self.header = header
-        self.message = message
-        self.display = pyfiglet.print_figlet(title)
-
-
-title = 'Python Scanner'
-description = 'Python Scanner is a simple port scanning application written in python'
-
-ban = banner('Main Banner', title, description)
